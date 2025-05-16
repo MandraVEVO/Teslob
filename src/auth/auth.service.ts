49e97c16f074383f1dc73c93709aac9d50e58199
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-05-15 19:39:54 -0600

    Implementacion de usuario, encriptacion de contraseña con bcrypt ademas de creacion de end-point de login y register validado

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,62 @@
+import { Injectable, BadRequestException, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
+
+
+@Injectable()
+export class AuthService {
+constructor(
+  @InjectRepository(User)
+  private readonly userRepository: Repository<User>,  
+) {}
+
+
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const {password, ...userDate } = createUserDto;
+      
+      const user = this.userRepository.create({
+        ...userDate,
+        password: bcrypt.hashSync(password, 10),
+      });
+      await this.userRepository.save(user);
+
+      return user;
+      //TODO return the JWT of access
+  
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const {password, email} = loginUserDto;
+    const user = await this.userRepository.findOne({
+       where: {email},
+       select: {email: true, password: true}
+      });
+      if(!user)
+         throw new UnauthorizedException('Credentials are not valid (email)');
+
+      if(!bcrypt.compareSync(password,user.password))
+        throw new UnauthorizedException('Credentials are not valid (password)');
+
+
+
+    return user;
+    //TO DO: RETORNAR EL JWT
+  }
+
+  private handleDBErrors(error: any): never{
+    if (error.code === '23505') 
+      throw new BadRequestException(error.detail);
+    console.log(error);
+    throw new InternalServerErrorException('Please check server logs');
+  }
+
+}

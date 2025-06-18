5b39bfe8ff83a1a1ccb904534e38029903a75121
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-06-18 09:32:30 -0600

    Implementacion de websocket gateway

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,7 +1,21 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({cors: true})
-export class MessageWsGateway {
+export class MessageWsGateway  implements OnGatewayConnection, OnGatewayDisconnect{
   constructor(private readonly messageWsService: MessageWsService) {}
+
+    handleConnection(client: Socket ) {
+    // You can store connected clients or log the connection
+    console.log(`Client connected`, client.id);
+    
+  }
+
+  handleDisconnect(client: Socket) {
+    // Handle client disconnection logic
+    console.log(`Client disconnected:`,client.id);
+    
+  }
+
 }

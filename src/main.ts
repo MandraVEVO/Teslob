2b4d665acb6f84cf87cbda43d73fc317d14139dd
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-06-17 22:49:30 -0600

    Implementacion de uso de SWAGGER para documentacion de API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,15 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true,
   }))
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo RESTFul example')
+    .setDescription('TesloShop endpoints')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`Application is running on: ${process.env.PORT}`);

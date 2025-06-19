d3cb73244351e1a94908291771bbd6cd7da198f1
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-06-19 11:14:12 -0600

    configuracion para deploy en produccion en heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { MessageWsModule } from './message-ws/message-ws.module';
+import { rejects } from 'assert';
 
 
 
@@ -17,6 +18,13 @@ import { MessageWsModule } from './message-ws/message-ws.module';
     ConfigModule.forRoot(),
 
    TypeOrmModule.forRoot({
+    ssl: process.env.STAGE === 'prod',
+    extra: {
+      ssl: process.env.STAGE === 'prod'
+    ?{ rejectUnauthorized: false}
+    :null,
+    },
+
     type: 'postgres',
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT!,

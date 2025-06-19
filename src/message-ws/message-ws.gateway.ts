9eb23ccbe8f8fe430a7c2a970b23e04b6fe4a2a6
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-06-18 21:04:17 -0600

    Formas de emitir desde el servidor

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,21 +1,52 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from 'src/common/dto/new-message.dto';
 
 @WebSocketGateway({cors: true})
 export class MessageWsGateway  implements OnGatewayConnection, OnGatewayDisconnect{
+
+
+  @WebSocketServer() wss: Server
   constructor(private readonly messageWsService: MessageWsService) {}
 
     handleConnection(client: Socket ) {
     // You can store connected clients or log the connection
-    console.log(`Client connected`, client.id);
-    
+    // console.log(`Client connected`, client.id);
+    this.messageWsService.registerClient(client);
+
+    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
+
   }
 
   handleDisconnect(client: Socket) {
     // Handle client disconnection logic
-    console.log(`Client disconnected:`,client.id);
-    
+    // console.log(`Client disconnected:`,client.id);
+    this.messageWsService.removeClient(client.id);
+    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
   }
+
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client: Socket, payload: NewMessageDto) {
+    
+    //!emite unicamente al cliente
+    // client.emit('message-from-server', {
+    //   fullName: 'Soy yo',
+    //   message:payload.message || 'No message provided',
+    // })
+
+    //!emite a todos los clientes conectados menos al que emitio
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: 'Soy yo',
+    //   message:payload.message || 'No message provided',
+    // })
+
+    //emite a todos los clientes
+    this.wss.emit('message-from-server', {
+      fullName: 'soy yo',
+      message: payload.message || 'No message provided',
+    });
+
+    };
 
 }
--- src/message-ws/message-ws.service.ts
+++ src/message-ws/message-ws.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
 
+interface ConnectedClients {
+    [id: string]: Socket
+}
+
+{
+
+}
 @Injectable()
-export class MessageWsService {}
+export class MessageWsService {
+    private connectedClients:ConnectedClients = {}
+
+    registerClient(client: Socket){
+        this.connectedClients[client.id] = client;
+    }
+
+    removeClient(clientId: string) {
+        delete this.connectedClients[clientId];
+
+    }
+
+    getConnectedClients(): string[] {
+        return Object.keys(this.connectedClients);
+    }
+
+}
